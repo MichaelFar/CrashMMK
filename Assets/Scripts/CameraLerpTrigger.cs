15b86229d0d0f925e8ca1f091c23bcdd5e2a84ae
MichaelFar <rarrafarrar@gmail.com>
2024-11-07 15:46:11 -0800

    Improved Camera, not perfect but is sufficient

---
+++ Assets/Scripts/CameraLerpTrigger.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CameraLerpTrigger: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+
+    public float CameraSpeed = 0.1f;
+
+    private float alpha = 0.0f;
+
+    private bool InTriggerZone = false;
+
+    private Vector3 OriginalPositionOfCamera;
+    void Start()
+    {
+        OriginalPositionOfCamera = transform.position;
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        
+        var AlphaSlope = Time.deltaTime * 1.0f/20.0f;
+
+        var DestinationVector = new Vector3(0.0f, transform.position.y, player.transform.position.z - 5.0f);
+
+        alpha += AlphaSlope;
+
+        transform.position = Vector3.Lerp(transform.position, DestinationVector, alpha);
+        
+        alpha = Math.Clamp(alpha, 0.0f, 1.0f);
+
+        print("In trigger zone");
+        
+        print("Camera position is " + transform.position);
+
+        if(Vector3.Distance(transform.position, DestinationVector) < 0.8f)
+        {
+            alpha -= AlphaSlope;
+        }
+
+    }
+    private double easeOutQuart(double x)
+    {
+        return 1.0 - Math.Pow(1.0 - x, 4.0);
+    }
+
+}
