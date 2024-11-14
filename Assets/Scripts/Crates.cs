8bcb47130c78cec8e924fcf47ce78196497935f1
Matt3324 <jimenezmateo3324@gmail.com>
2024-11-14 14:38:47 -0800

    added crate functions
    a

--- Assets/Scripts/Crates.cs
+++ Assets/Scripts/Crates.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Mateo Jimenez
+ * 11/14/2024
+ * handles crate desctruction and spawning
+ */
+
 public class Crates : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int dropCount = 1;
+
+    public GameObject dropFruit;
+
+    
+    // Start is called before the first frame update
+
+    //crate is destroyed and spawns the amount of fruit from dropcount
+    public void CrateDestroy()
     {
-        
+
+        Destroy(gameObject);
+
+        for (int i = 0; i < dropCount; i++)
+        {
+            Instantiate(dropFruit, transform.position, Quaternion.identity);
+        }
     }
+    
+   
 }
